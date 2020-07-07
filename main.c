0554d88b22730c3370cdfd16ef8c57ca1a584510
Nomi Hariman <nhariman@Nomis-MacBook-Air.local>
2020-07-08 00:38:31 +0200

    make a regex function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/06 21:16:18 by nhariman      #+#    #+#                 */
-/*   Updated: 2020/07/06 22:49:49 by nhariman      ########   odam.nl         */
+/*   Updated: 2020/07/08 00:09:08 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,25 @@
 
 int		main(int argc, char **argv)
 {
+	char	*cubfile;
+
+	cubfile = argv[1] ? ft_strdup(argv[1]) : NULL;
 	if (argc > 3 || argc == 1)
 	{
 		ft_printf("Invalid amount of arguments\n");
-		exit(1);
 	}
-	else if (ft_strncmp(argv[1] + (ft_strlen(argv[1]) - 4), ".cub\0", 4))
+	else if (!ft_strnstr(cubfile, ".cub", ft_strlen(cubfile)))
 	{
 		ft_printf("no .cub file was received.");
-		exit(1);
 	}
 	else if (argc == 3 && (ft_strncmp(argv[2], "--save", 7)))
 	{
 		ft_printf("This program only takes --save as second argument\n");
-		exit(1);
 	}
 	else
 	{
 		ft_printf("yay i print smth.\n");
 	}
+	free(cubfile);
 	return (0);
 }
