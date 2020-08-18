9117479cce5a1fb51da01acaf969b65ff02d1a17
Niks Hariman <nhariman@Niks-MacBook-Air.local>
2020-08-19 00:55:45 +0200

    also forgot sprites

--- srcs/raycasting/sprites.c
+++ srcs/raycasting/sprites.c
@@ -6,7 +6,7 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/17 22:25:46 by nhariman      #+#    #+#                 */
-/*   Updated: 2020/08/17 22:30:51 by nhariman      ########   odam.nl         */
+/*   Updated: 2020/08/19 00:55:32 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void			get_sprites(t_cub *cub)
 {
 	cub->sp.zbuffer = ft_calloc(cub->res_x, sizeof(double));
-	
+	cub->sp.sp_order = ft_calloc(cub->sprites, sizeof(int));
+	cub->sp.sp_dist = ft_calloc(cub->sprites, sizeof(double));
 }
